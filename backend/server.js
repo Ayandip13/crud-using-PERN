5c1c7889536c99efe724c7f47bb3d37d2f52b11e
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2026-02-19 17:00:47 +0530

    feat: Set up initial Express backend server with middleware and a sample products API endpoint, including environment configuration.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,39 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
+app.use(express.json()); //it's a middleware that helps us to parse the JSON data from the request body
+app.use(helmet()); //it's a security middleware that heelps us to protect our app by setting various HTTP headers
+app.use(morgan("dev")); //log the requests
+app.use(cors()); //it's a middleware that helps us to allow the requests from the different origins
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/api/products", (req, res) => {
+    //get all the products from the database
+    res.status(200).json({
+        success: true,
+        data: [
+            {
+                id: 1,
+                name: "Product 1",
+            },
+            {
+                id: 2,
+                name: "Product 2",
+            },
+            {
+                id: 3,
+                name: "Product 3",
+            }
+        ]
+    })
 });
 
 app.listen(PORT, () => {
