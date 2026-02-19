e8cb9df6761dc1037efd5d7d645113673a7fd9f7
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2026-02-20 03:23:41 +0530

    Implement all pages and styles

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dracula",
+      "cmyk",
+      "business",
+      "acid",
+      "lemon",
+    ],
+  },
+}
